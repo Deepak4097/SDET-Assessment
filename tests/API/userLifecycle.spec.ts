34618fbe96c9f1d89965209bd2d78e3c5e57f10f
Deepak4097 <awardeepu@gmail.com>
2026-07-10 19:14:49 +0530

    updated assertions

--- tests/API/userLifecycle.spec.ts
+++ tests/API/userLifecycle.spec.ts
@@ -28,27 +28,39 @@ test.describe("User Lifecycle API", () => {
         expect(createResponse.message).toBe("User created!");
 
         // TA08 - Get User Details By Email
-        // TA08 - Get User Details By Email
+
         const getUserResponse = await userApi.getUserDetailByEmail(user.email);
 
         expect(getUserResponse.responseCode).toBe(200);
 
-        expect(getUserResponse.user.email).toBe(user.email);
+        expect(getUserResponse.user).toBeDefined();
+        expect(getUserResponse.user.id).toEqual(expect.any(Number));
+        expect(getUserResponse.user.id).toBeGreaterThan(0);
+
+        // Personal Details
         expect(getUserResponse.user.name).toBe(user.name);
+        expect(getUserResponse.user.email).toBe(user.email);
         expect(getUserResponse.user.title).toBe(user.title);
 
+        // Name Details
         expect(getUserResponse.user.first_name).toBe(user.firstname);
         expect(getUserResponse.user.last_name).toBe(user.lastname);
 
+        // DOB
+        expect(getUserResponse.user.birth_day).toBe(user.birth_date);
+        expect(getUserResponse.user.birth_month).toBe(user.birth_month);
+        expect(getUserResponse.user.birth_year).toBe(user.birth_year);
+
+        // Company & Address
         expect(getUserResponse.user.company).toBe(user.company);
         expect(getUserResponse.user.address1).toBe(user.address1);
         expect(getUserResponse.user.address2).toBe(user.address2);
 
+        // Location
         expect(getUserResponse.user.country).toBe(user.country);
         expect(getUserResponse.user.state).toBe(user.state);
         expect(getUserResponse.user.city).toBe(user.city);
         expect(getUserResponse.user.zipcode).toBe(user.zipcode);
-        
         // TA09 - Delete Account
         const deleteResponse = await userApi.deleteAccount(
             user.email,
